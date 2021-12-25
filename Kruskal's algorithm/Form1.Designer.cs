cd55461efe09c35b656c17cc3c42cfbd2b6e4a67
Евгений Райман <021113551157@enu.kz>
2021-12-25 14:52:22 +0600

    Добавьте файлы проекта.

---
+++ Kruskal's algorithm/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace Kruskal_s_algorithm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Solve = new System.Windows.Forms.Button();
+            this.Sort = new System.Windows.Forms.Button();
+            this.ForSorting = new System.Windows.Forms.DataGridView();
+            this.CloseForm = new System.Windows.Forms.Button();
+            this.ForDecision = new System.Windows.Forms.DataGridView();
+            this.ForArcs = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ForSorting)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ForDecision)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ForArcs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Solve
+            // 
+            this.Solve.Location = new System.Drawing.Point(12, 12);
+            this.Solve.Name = "Solve";
+            this.Solve.Size = new System.Drawing.Size(88, 40);
+            this.Solve.TabIndex = 0;
+            this.Solve.Text = "Решить";
+            this.Solve.UseVisualStyleBackColor = true;
+            this.Solve.Click += new System.EventHandler(this.Solve_Click);
+            // 
+            // Sort
+            // 
+            this.Sort.Location = new System.Drawing.Point(106, 12);
+            this.Sort.Name = "Sort";
+            this.Sort.Size = new System.Drawing.Size(88, 40);
+            this.Sort.TabIndex = 1;
+            this.Sort.Text = "Сортировать";
+            this.Sort.UseVisualStyleBackColor = true;
+            this.Sort.Click += new System.EventHandler(this.Sort_Click);
+            // 
+            // ForSorting
+            // 
+            this.ForSorting.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ForSorting.Location = new System.Drawing.Point(200, 12);
+            this.ForSorting.Name = "ForSorting";
+            this.ForSorting.Size = new System.Drawing.Size(210, 211);
+            this.ForSorting.TabIndex = 2;
+            // 
+            // CloseForm
+            // 
+            this.CloseForm.Location = new System.Drawing.Point(12, 58);
+            this.CloseForm.Name = "CloseForm";
+            this.CloseForm.Size = new System.Drawing.Size(88, 40);
+            this.CloseForm.TabIndex = 3;
+            this.CloseForm.Text = "Выход";
+            this.CloseForm.UseVisualStyleBackColor = true;
+            this.CloseForm.Click += new System.EventHandler(this.CloseForm_Click);
+            // 
+            // ForDecision
+            // 
+            this.ForDecision.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ForDecision.Location = new System.Drawing.Point(416, 12);
+            this.ForDecision.Name = "ForDecision";
+            this.ForDecision.Size = new System.Drawing.Size(160, 211);
+            this.ForDecision.TabIndex = 4;
+            // 
+            // ForArcs
+            // 
+            this.ForArcs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ForArcs.Location = new System.Drawing.Point(582, 12);
+            this.ForArcs.Name = "ForArcs";
+            this.ForArcs.Size = new System.Drawing.Size(260, 211);
+            this.ForArcs.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(200, 229);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(592, 397);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1304, 749);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ForArcs);
+            this.Controls.Add(this.ForDecision);
+            this.Controls.Add(this.CloseForm);
+            this.Controls.Add(this.ForSorting);
+            this.Controls.Add(this.Sort);
+            this.Controls.Add(this.Solve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ForSorting)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ForDecision)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ForArcs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Solve;
+        private System.Windows.Forms.Button Sort;
+        private System.Windows.Forms.DataGridView ForSorting;
+        private System.Windows.Forms.Button CloseForm;
+        private System.Windows.Forms.DataGridView ForDecision;
+        private System.Windows.Forms.DataGridView ForArcs;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Kruskal's algorithm/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kruskal_s_algorithm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int[,] Arcs = new int[,]
+           {
+            //{2,3,3,0 },
+            //{3,4,5,0 },
+            //{2,4,11,0 },
+            //{2,1,20,0 },
+            //{1,4,1,0 },
+            //{2,5,23,0 },
+            //{5,3,13,0 },
+            //{3,8,8,0 },
+            //{4,8,2,0 },
+            //{4,7,7,0 },
+            //{7,8,5,0 },
+            //{7,1,3,0 },
+            //{7,9,11,0 },
+            //{1,9,45,0 },
+            //{1,6,16,0 },
+            //{9,6,3,0 },
+            //{2,6,8,0 },
+
+
+               {1,6,65,0 },
+            {1,19,10 ,0},
+            {19,17,35,0},
+            {17,15,5,0},
+            {15,9,65,0},
+            {19,10,70 ,0},
+            {17,20,50 ,0},
+            {15,16,15 ,0},
+            {9,5,45 ,0},
+            {6,10,15 ,0},
+            {10,20,10,0 },
+            {20,16,20 ,0},
+            {16,5,15,0 },
+            {6,8,70,0 },
+            {10,18,50 ,0 },
+            {20,2,75,0 },
+            {16,7,27,0 },
+            {5,11,75 ,0},
+            {8,18,5,0 },
+            {18,2,15 ,0},
+            {2,7,50 ,0},
+            {7,11,65 ,0 },
+            {8,3,46 ,0},
+            {18,13,76 ,0},
+            {2,4,34,0},
+            {7,12,19,0 },
+            {11,14,13,0 },
+            {3,13,23,0 },
+            {13,4,54,0 },
+            {4,12,12 ,0},
+            {12,14,67 ,0},
+            {3,21,37,0},
+            {21,22,97 ,0},
+            {13,22,19 ,0},
+            {22,23,27,0},
+            {4,23,22 ,0},
+            {23,24,54 ,0 },
+            {12,24,34,0},
+            {24,25,76,0},
+            {25,14,57,0},
+            {7,5,100,0},
+            {1,10,100,0},
+           };
+        public int[,] SortArcs = new int[,]
+             {
+
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0 ,0},
+            {0,0,0,0 ,0},
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0 ,0},
+            {0,0,0,0 ,0},
+            {0,0,0,0 ,0},
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0 ,0},
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0,0 },
+            {0,0,0,0 ,0},
+           };
+        public int[,] Nodes = new int[,]
+        {
+            {1,0,0},
+            {2,0,0},
+            {3,0,0 },
+            { 4,0,0},
+            { 5,0,0},
+            { 6,0,0},
+            { 7,0,0},
+            { 8,0,0},
+            { 9,0,0},
+            { 10,0,0},
+            { 11,0,0},
+            { 12,0,0},
+            { 13,0,0},
+            { 14,0,0},
+            { 15,0,0},
+            { 16,0,0},
+            { 17,0,0},
+            { 18,0,0},
+            { 19,0,0},
+            { 20,0,0},
+            { 21,0,0},
+            { 22,0,0},
+            { 23,0,0},
+            { 24,0,0},
+            { 25,0,0},
+};
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ForSorting.Visible = false;
+            ForArcs.RowCount = 42;
+            ForArcs.ColumnCount = 4;
+
+            ForArcs.Columns[0].Width = 50;
+            ForArcs.Columns[1].Width = 50;
+            ForArcs.Columns[2].Width = 50;
+            ForArcs.Columns[3].Width = 50;
+            ForArcs.Columns[0].HeaderCell.Value = "From";
+            ForArcs.Columns[1].HeaderCell.Value = "To";
+            ForArcs.Columns[2].HeaderCell.Value = "Length";
+            ForArcs.Columns[3].HeaderCell.Value = "Flag";
+            for (int i=0;i<42;i++)
+            {
+                for (int j=0;j<3;j++)
+                {
+                    ForArcs.Rows[i].Cells[j].Value = Arcs[i, j];
+                }
+            }
+            ForDecision.RowCount = 25;
+
+            ForDecision.ColumnCount = 2;
+            
+            ForDecision.Columns[0].Width = 50;
+            ForDecision.Columns[1].Width = 50;
+            for (int i=1;i<=25;i++)
+            {
+                ForDecision.Rows[i-1].Cells[0].Value = i;
+            }
+            ForDecision.Columns[0].HeaderCell.Value = "Node";
+            ForDecision.Columns[1].HeaderCell.Value = "Tree";
+        }
+
+        private void Solve_Click(object sender, EventArgs e)
+        {
+            int nU = 25, nD = 42, treescount = 0, treesnumber = 0, usednodes = 0, From = 0, To = 0, FromTree = 0, ToTree = 0;
+
+            while (true)
+            {
+                int minelement = 10000, minelementposition = 0;
+                for (int i = 0; i < nD; i++)
+                {
+                    int flag = Convert.ToInt32(ForArcs.Rows[i].Cells[3].Value);
+                    if (flag == 0)
+                    {
+                        int tekL = Convert.ToInt32(ForArcs.Rows[i].Cells[2].Value);
+                        if (tekL < minelement)
+                        {
+                            minelement = tekL;
+                            minelementposition = i;
+                        }
+                        From = Convert.ToInt32(ForArcs.Rows[minelementposition].Cells[0].Value);
+                        To = Convert.ToInt32(ForArcs.Rows[minelementposition].Cells[1].Value);
+                        FromTree = Convert.ToInt32(ForDecision.Rows[From - 1].Cells[1].Value);
+                        ToTree = Convert.ToInt32(ForDecision.Rows[To - 1].Cells[1].Value);
+                    }
+                }
+
+
+                if (FromTree == 0 && ToTree == 0)
+                {
+                    treescount++;
+                    treesnumber++;
+                    usednodes = usednodes + 2;
+                    ForDecision.Rows[To - 1].Cells[1].Value = treescount;
+                    ForDecision.Rows[From - 1].Cells[1].Value = treescount;
+                    ForArcs.Rows[minelementposition].Cells[3].Value = 1;
+                }
+                else if ((FromTree != 0 && ToTree == 0) || (FromTree == 0 && ToTree != 0))
+                {
+                    usednodes++;
+                    if (FromTree == 0)
+                    {
+                        ForDecision.Rows[From - 1].Cells[1].Value = ToTree;
+                    }
+                    else if (ToTree == 0)
+                    {
+                        ForDecision.Rows[To - 1].Cells[1].Value = FromTree;
+                    }
+
+                    ForArcs.Rows[minelementposition].Cells[3].Value = 1;
+                }
+                else if (FromTree != 0 && ToTree != 0 && FromTree != ToTree)
+                {
+                    treescount--;
+                    for (int i = 0; i < nU; i++)
+                    {
+                        int treenum = Convert.ToInt32(ForDecision.Rows[i].Cells[1].Value);
+                        if (treenum == ToTree)
+                        {
+                            ForDecision.Rows[i].Cells[1].Value = FromTree;
+                        }
+                    }
+                    ForArcs.Rows[minelementposition].Cells[3].Value = 1;
+                }
+                else if (FromTree == ToTree)
+                {
+                    ForArcs.Rows[minelementposition].Cells[3].Value = 2;
+                }
+                if (treescount == 1 && usednodes == nU)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i < nD; i++)
+            {
+                int flag = Convert.ToInt32(ForArcs.Rows[i].Cells[3].Value);
+                if (flag == 1)
+                {
+                    ForArcs.Rows[i].DefaultCellStyle.BackColor = Color.Green;
+                }
+                if (flag == 2)
+                {
+                    ForArcs.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                }
+            }
+
+
+        }
+
+        private void Sort_Click(object sender, EventArgs e)
+        {
+            int nU = 25, treescount = 0, treesnumber = 0, usednodes = 0, From = 0, To = 0, FromTree = 0, ToTree = 0, minposition = 0;
+            ForSorting.Visible = true;
+            int min = 10000, nD = 42, count=0, minIndex=0;
+            while (count!=nD)
+            {
+                for (int i = 0; i < nD; i++)
+                {
+                    
+                    if (Arcs[i, 2] <= min)
+                    {
+
+                        if (Arcs[i, 3] == 0)
+                        {
+                            min = Arcs[i, 2];
+                            minIndex = i;
+                            for (int k = 0; k < 4; k++)
+                            {
+                                SortArcs[count, k] = Arcs[minIndex, k];
+                            }
+                        }
+                        
+                        
+                    }
+                }
+                Arcs[minIndex, 3] = 1;
+                min = 10000;
+                count++;
+                
+            }
+            ForSorting.RowCount = nD;
+            ForSorting.ColumnCount = 5;
+            ForSorting.Columns[0].HeaderCell.Value = "Begin";
+            ForSorting.Columns[1].HeaderCell.Value = "End";
+            ForSorting.Columns[2].HeaderCell.Value = "Length";
+            ForSorting.Columns[3].HeaderCell.Value = "Flag";
+            ForSorting.Columns[0].Width = 50;
+            ForSorting.Columns[1].Width = 50;
+            ForSorting.Columns[2].Width = 50;
+            ForSorting.Columns[3].Width = 50;
+            for (int i=0; i<nD;i++)
+            {
+                for (int j=0;j<4;j++)
+                {
+                    ForSorting.Rows[i].Cells[j].Value = SortArcs[i, j];
+                }
+                
+            }
+            bool check = true;
+            while (check == true)
+            {
+                for (int i = 0; i < nD; i++)
+                {
+                    int flag = Convert.ToInt32(ForSorting.Rows[i].Cells[3].Value);
+                    if (flag == 0)
+                    {
+                        minposition = i;
+                        From = Convert.ToInt32(ForSorting.Rows[i].Cells[0].Value);
+                        To = Convert.ToInt32(ForSorting.Rows[i].Cells[1].Value);
+                        FromTree = Convert.ToInt32(ForDecision.Rows[From - 1].Cells[1].Value);
+                        ToTree = Convert.ToInt32(ForDecision.Rows[To - 1].Cells[1].Value);
+                        break;
+                    }
+                }
+
+
+                if (FromTree == 0
+                    && ToTree == 0)
+                {
+                    treescount++; treesnumber++;
+                    usednodes = usednodes + 2;
+
+                    ForDecision.Rows[To - 1].Cells[1].Value = treescount;
+                    ForDecision.Rows[From - 1].Cells[1].Value = treescount;
+
+                    ForSorting.Rows[minposition].Cells[3].Value = 1;
+                }
+                else if ((FromTree != 0 && ToTree == 0)
+                    || (FromTree == 0 && ToTree != 0))
+                {
+                    usednodes++;
+
+                    if (FromTree == 0)
+                    {
+                        ForDecision.Rows[From - 1].Cells[1].Value = ToTree;
+                    }
+
+                    else if (ToTree == 0)
+                    {
+                        ForDecision.Rows[To - 1].Cells[1].Value = FromTree;
+                    }
+
+                    ForSorting.Rows[minposition].Cells[3].Value = 1;
+                }
+                else if (FromTree != 0 && ToTree != 0 && FromTree != ToTree)
+                {
+                    treescount--;
+                    for (int i = 0; i < nU; i++)
+                    {
+                        int treenum = Convert.ToInt32(ForDecision.Rows[i].Cells[1].Value);
+                        if (treenum == ToTree)
+                        {
+                            ForDecision.Rows[i].Cells[1].Value = FromTree;
+                        }
+                    }
+
+                    ForSorting.Rows[minposition].Cells[3].Value = 1;
+                }
+                else if (FromTree == ToTree)
+                {
+                    ForSorting.Rows[minposition].Cells[3].Value = 2;
+                }
+
+                if (treescount == 1 && usednodes == nU)
+                {
+                    check = false;
+                }
+            }
+            for (int i = 0; i < nD; i++)
+            {
+                int flag = Convert.ToInt32(ForSorting.Rows[i].Cells[4].Value);
+                if (flag == 1)
+                {
+                    ForSorting.Rows[i].Cells[0].Style.BackColor = Color.Green;
+                }
+                if (flag == 2)
+                {
+                    ForSorting.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                }
+            }
+
+        }
+
+        private void CloseForm_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
